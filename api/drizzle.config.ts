7ad9b58848329d8e4b000c90c6bbb3cacb11edae
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-02-21 19:16:19 +0300

    drizzle setup

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema.ts',
+  // schema: './src/db/schema.ts',
+  schema: ['./src/db/productsSchema.ts'],
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
