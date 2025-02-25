73e3d2ed6d4999250af13d131f5ba523f6cf4d20
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-02-25 13:35:50 +0300

    now we register user

--- api/src/routes/auth/index.ts
+++ api/src/routes/auth/index.ts
@@ -1,5 +1,27 @@
 import { Router } from "express";
+import { validateData } from "../../middlewares/validationMiddleware";
+import { createUserSchema, usersTable } from "../../db/usersSchema";
+import bcrypt from 'bcryptjs';
+import { db } from '../../db/index';
 
 const router = Router();
+
+router.post('/register', validateData(createUserSchema), async (req, res) => {
+    try {
+    // console.log(req.cleanBody);
+    const data = req.cleanBody;
+    // const hashedPassword = await bcrypt.hash(data.password, 10);
+    // console.log(data, hashedPassword);
+    data.password = await bcrypt.hash(data.password, 10);
+    const [user] = await db.insert(usersTable).values(data).returning();
+
+    res.status(200).json({ user });
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
+router.post('/login', (req, res) => {
+    res.status(200).send("Login route");
+});
 
 export default router;
